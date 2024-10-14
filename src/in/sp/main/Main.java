25ac84f86d37c3ea51c7cb2759a6a41738c53a6a
ShekhawatCodes <sumitshekhawatwork@gmail.com>
2024-10-14 16:27:14 +0530

    Initial commit

---
+++ src/in/sp/main/Main.java
@@ -0,0 +1,18 @@
+package in.sp.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.sp.beans.Student;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		String config_file_loc = "/in/sp/resources/ApplicationContext.xml";
+	ApplicationContext context = new ClassPathXmlApplicationContext(config_file_loc);
+	
+	 Student std = (Student)context.getBean("stdId");
+	 std.display();
+	}
+
+}
